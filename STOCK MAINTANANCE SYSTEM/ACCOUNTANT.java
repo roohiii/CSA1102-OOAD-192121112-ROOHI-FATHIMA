8a406a2b6dbcc30e4833a4955a2e86b28d98c67b
Roohi Fathima[192121112] <112243951+roohiii@users.noreply.github.com>
2023-08-19 10:32:05 +0530

    Add files via upload

---
+++ STOCK MAINTANANCE SYSTEM/ACCOUNTANT.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class ACCOUNTANT
+ */
+public class ACCOUNTANT {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void id;
+  
+  //
+  // Constructors
+  //
+  public ACCOUNTANT () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (void newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private void getId () {
+    return id;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void prepare_invoice()
+  {
+  }
+
+
+  /**
+   */
+  public void update()
+  {
+  }
+
+
+}
---
+++ STOCK MAINTANANCE SYSTEM/CUSTOMER.java
@@ -0,0 +1,103 @@
+
+
+/**
+ * Class CUSTOMER
+ */
+public class CUSTOMER {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void phn_number;
+  private void address;
+  
+  //
+  // Constructors
+  //
+  public CUSTOMER () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of phn_number
+   * @param newVar the new value of phn_number
+   */
+  private void setPhn_number (void newVar) {
+    phn_number = newVar;
+  }
+
+  /**
+   * Get the value of phn_number
+   * @return the value of phn_number
+   */
+  private void getPhn_number () {
+    return phn_number;
+  }
+
+  /**
+   * Set the value of address
+   * @param newVar the new value of address
+   */
+  private void setAddress (void newVar) {
+    address = newVar;
+  }
+
+  /**
+   * Get the value of address
+   * @return the value of address
+   */
+  private void getAddress () {
+    return address;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void place_order()
+  {
+  }
+
+
+  /**
+   */
+  public void payment()
+  {
+  }
+
+
+  /**
+   */
+  public void recieve()
+  {
+  }
+
+
+}

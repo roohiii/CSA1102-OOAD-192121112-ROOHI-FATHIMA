156295275c8655766b6bf8cdd1ae70185c3d2172
Roohi Fathima[192121112] <112243951+roohiii@users.noreply.github.com>
2023-08-21 19:14:29 +0530

    Add files via upload

---
+++ E-COMMERCE WEBSITES/ORDER.java
@@ -0,0 +1,174 @@
+
+
+/**
+ * Class ORDER
+ */
+public class ORDER {
+
+  //
+  // Fields
+  //
+
+  private void id;
+  private void seller_id;
+  private void customer_id;
+  private void product_id;
+  private void total_amount;
+  private void order_data;
+  private void delivery_data;
+  private void delivery_status;
+  
+  //
+  // Constructors
+  //
+  public ORDER () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (void newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private void getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of seller_id
+   * @param newVar the new value of seller_id
+   */
+  private void setSeller_id (void newVar) {
+    seller_id = newVar;
+  }
+
+  /**
+   * Get the value of seller_id
+   * @return the value of seller_id
+   */
+  private void getSeller_id () {
+    return seller_id;
+  }
+
+  /**
+   * Set the value of customer_id
+   * @param newVar the new value of customer_id
+   */
+  private void setCustomer_id (void newVar) {
+    customer_id = newVar;
+  }
+
+  /**
+   * Get the value of customer_id
+   * @return the value of customer_id
+   */
+  private void getCustomer_id () {
+    return customer_id;
+  }
+
+  /**
+   * Set the value of product_id
+   * @param newVar the new value of product_id
+   */
+  private void setProduct_id (void newVar) {
+    product_id = newVar;
+  }
+
+  /**
+   * Get the value of product_id
+   * @return the value of product_id
+   */
+  private void getProduct_id () {
+    return product_id;
+  }
+
+  /**
+   * Set the value of total_amount
+   * @param newVar the new value of total_amount
+   */
+  private void setTotal_amount (void newVar) {
+    total_amount = newVar;
+  }
+
+  /**
+   * Get the value of total_amount
+   * @return the value of total_amount
+   */
+  private void getTotal_amount () {
+    return total_amount;
+  }
+
+  /**
+   * Set the value of order_data
+   * @param newVar the new value of order_data
+   */
+  private void setOrder_data (void newVar) {
+    order_data = newVar;
+  }
+
+  /**
+   * Get the value of order_data
+   * @return the value of order_data
+   */
+  private void getOrder_data () {
+    return order_data;
+  }
+
+  /**
+   * Set the value of delivery_data
+   * @param newVar the new value of delivery_data
+   */
+  private void setDelivery_data (void newVar) {
+    delivery_data = newVar;
+  }
+
+  /**
+   * Get the value of delivery_data
+   * @return the value of delivery_data
+   */
+  private void getDelivery_data () {
+    return delivery_data;
+  }
+
+  /**
+   * Set the value of delivery_status
+   * @param newVar the new value of delivery_status
+   */
+  private void setDelivery_status (void newVar) {
+    delivery_status = newVar;
+  }
+
+  /**
+   * Get the value of delivery_status
+   * @return the value of delivery_status
+   */
+  private void getDelivery_status () {
+    return delivery_status;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void place_order()
+  {
+  }
+
+
+}
---
+++ E-COMMERCE WEBSITES/PRODUCT.java
@@ -0,0 +1,144 @@
+
+
+/**
+ * Class PRODUCT
+ */
+public class PRODUCT {
+
+  //
+  // Fields
+  //
+
+  private void productID;
+  private void product_name;
+  private void product_cost;
+  private void seller_id;
+  private void posted_data;
+  
+  //
+  // Constructors
+  //
+  public PRODUCT () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of productID
+   * @param newVar the new value of productID
+   */
+  private void setProductID (void newVar) {
+    productID = newVar;
+  }
+
+  /**
+   * Get the value of productID
+   * @return the value of productID
+   */
+  private void getProductID () {
+    return productID;
+  }
+
+  /**
+   * Set the value of product_name
+   * @param newVar the new value of product_name
+   */
+  private void setProduct_name (void newVar) {
+    product_name = newVar;
+  }
+
+  /**
+   * Get the value of product_name
+   * @return the value of product_name
+   */
+  private void getProduct_name () {
+    return product_name;
+  }
+
+  /**
+   * Set the value of product_cost
+   * @param newVar the new value of product_cost
+   */
+  private void setProduct_cost (void newVar) {
+    product_cost = newVar;
+  }
+
+  /**
+   * Get the value of product_cost
+   * @return the value of product_cost
+   */
+  private void getProduct_cost () {
+    return product_cost;
+  }
+
+  /**
+   * Set the value of seller_id
+   * @param newVar the new value of seller_id
+   */
+  private void setSeller_id (void newVar) {
+    seller_id = newVar;
+  }
+
+  /**
+   * Get the value of seller_id
+   * @return the value of seller_id
+   */
+  private void getSeller_id () {
+    return seller_id;
+  }
+
+  /**
+   * Set the value of posted_data
+   * @param newVar the new value of posted_data
+   */
+  private void setPosted_data (void newVar) {
+    posted_data = newVar;
+  }
+
+  /**
+   * Get the value of posted_data
+   * @return the value of posted_data
+   */
+  private void getPosted_data () {
+    return posted_data;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void add_to_cart()
+  {
+  }
+
+
+  /**
+   */
+  public void sell_product()
+  {
+  }
+
+
+  /**
+   */
+  public void get_product()
+  {
+  }
+
+
+  /**
+   */
+  public void buy_product()
+  {
+  }
+
+
+}

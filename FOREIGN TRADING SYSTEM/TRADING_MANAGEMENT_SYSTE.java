27c3f7ca8ee3be86c2c36f5f59e4723454fb22f9
Roohi Fathima[192121112] <112243951+roohiii@users.noreply.github.com>
2023-08-21 18:54:28 +0530

    Add files via upload

---
+++ FOREIGN TRADING SYSTEM/TRADING_MANAGEMENT_SYSTE.java
@@ -0,0 +1,86 @@
+
+
+/**
+ * Class TRADING_MANAGEMENT_SYSTE
+ */
+public class TRADING_MANAGEMENT_SYSTE {
+
+  //
+  // Fields
+  //
+
+  private void verify_product;
+  private void amount;
+  
+  //
+  // Constructors
+  //
+  public TRADING_MANAGEMENT_SYSTE () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verify_product
+   * @param newVar the new value of verify_product
+   */
+  private void setVerify_product (void newVar) {
+    verify_product = newVar;
+  }
+
+  /**
+   * Get the value of verify_product
+   * @return the value of verify_product
+   */
+  private void getVerify_product () {
+    return verify_product;
+  }
+
+  /**
+   * Set the value of amount
+   * @param newVar the new value of amount
+   */
+  private void setAmount (void newVar) {
+    amount = newVar;
+  }
+
+  /**
+   * Get the value of amount
+   * @return the value of amount
+   */
+  private void getAmount () {
+    return amount;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void transport()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery_product()
+  {
+  }
+
+
+  /**
+   */
+  public void money_transfer()
+  {
+  }
+
+
+}
